66e9cd4418616f27d63e00abd221647eb64bf90a
souju <soujanyamupparapu@gmail.com>
2024-12-11 22:20:05 +0530

    operaters

---
+++ oepr.js
@@ -0,0 +1,71 @@
+// var a = 2;
+//  b = 3;
+// a= a+2 ;
+//  b= 3
+//  console.log(a);
+// console.log(a+b-7);
+
+
+
+
+// var a =5;
+// b = 3 ;
+//   a=a-3;     
+//  b= a-1  
+// console.log(b);
+
+
+// var a=5;
+// b=2;
+//  a=a*b;
+// //   5*2=10
+
+//  console.log(a);
+//  b=b*b;
+// //   2*2=4
+//  console.log(b);
+
+ 
+
+//  var a=8;
+//  b=4;
+//  a=a/b;
+// //    8/4=2
+//  console.log(a/2);
+//   b=4/2;
+//   b=8/b;
+//  console.log(b)
+ 
+//   var a=4;
+//   b=4;
+//   a= 16%4;
+//   console.log(a);
+//   b=10%5;
+//   console.log(b);
+ 
+ 
+ 
+ 
+// //  logical operators
+// //  &&And
+//  var a=30;
+//  b=20;
+//  console.log(a>b&&a>b);
+// OR
+var a=20;
+b=10;
+console.log(a<b||a<b);
+
+
+
+// !!NOT
+
+
+var a=20;
+console.log(!!a);
+b=-100;
+console.log(!!b);
+b=100;
+console.log(!!b);
+a=30;
+console.log(!!a)
